8a108ece194caf37129bc691adf83a7ea8278494
Kachel Wittig <kjwittig@gmail.com>
2018-02-05 07:57:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b
 }
+
+//inc(n) increments n and returns the result
+
+function inc(n) {
+  return n++
+}
+
+function dec(n) {
+  return n--
+}
